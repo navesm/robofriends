3fb17bba361a2925396f535818d741da528fd6b0
Steven NAVE <stevennave@gmail.com>
2022-12-15 15:32:12 -0500

    update

---
+++ src/components/CreateRobot.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+// const CreateRobot = ({name ,email, id}) => {
+  
+// }
+
+// export default CreateRobot;
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
+// import CreateRobot from '../components/CreateRobot';
 import './App.css'
 
 
